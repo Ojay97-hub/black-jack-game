7942b3836f122e556af9c11cb753a61cf8d39d71
Owen Cotter <owenjames97@outlook.com>
2024-05-15 15:17:31 +0000

    added the function flow for the game

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
 # import random for shuffling the cards
 import random 
 
+# Using balsamiq I have created a function flow for the game 
+# functions follow the logic of each stage of the game 
+# I have added the function layout below
+
+def display_rules():
+
+def create_deck():
+
+def deal_cards():
+
+def players_turn():
+
+def dealers_turn():
+
+def calculate_hand()
+
+def calculate_winner():
+
+def show_winner():
+
+def play_again():
