9f2c21c758ef243702c211465d687296e95872c1
Owen Cotter <owenjames97@outlook.com>
2024-05-17 12:27:34 +0000

    Added players turn function and fixed run.py issue of capitalisation

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def display_rules():
     print("To win and beat the dealer you need to get to 21 without going over.")
     print("Card values of King, Queen, and Jack are worth 10.")
     print("Ace can be worth either 1 or 11.")
-    print("other cards are worth their presented number.")
+    print("Other cards are worth their presented number.")
     print("Hit will grant you another card.")
     print("Stand will keep your current hand.")
     print("The dealer will hit until they reach 17 or higher.")
@@ -35,8 +35,7 @@ def display_rules():
 
 
 def create_deck():
-    values = ['ace', 'king', 'queen', 'jack', '10', '9',
-              '8', '7', '6', '5', '4', '3', '2']
+    values = ['Ace', 'King', 'Queen', 'Jack', '10', '9', '8', '7', '6', '5', '4', '3', '2']
     suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
     # making the 52 cards
     deck = [{'value': value, 'suit': suit} for value in values for suit in suits]
@@ -96,13 +95,15 @@ def deal_cards(deck, nums_cards=2):
     print("\nYour hand:")
     for card in player_hand:
         print(f"{card['value']} of {card['suit']}")
+    # call dealers turn
+    dealers_turn(deck, dealer_hand)
     return player_hand, dealer_hand
 
 
 """
 - This is the dealers turn function
 - While loop
-- Appending the deck 
+- Appending the deck
 
 """
 
@@ -111,11 +112,40 @@ def dealers_turn(deck, dealer_hand):
     # if dealers hand is less than 17 dealer will hit
     while calculate_hand(dealer_hand) < 17:
         dealer_hand.append(deck.pop())
+    # calling players turn
+    players_turn(deck, dealer_hand)
     return dealer_hand
 
 
+"""
+- This is the players turn function
+- Adds an action input for hit or stand
+- While loop
+- Appending the deck
 
-# def players_turn:
+"""
+
+
+def players_turn(deck, player_hand):
+    while True:
+        action = input("Do you want to hit or stand? (h/s): ").lower()
+        if action == 'h':
+            player_hand.append(deck.pop())
+            print("\nYour hand: ")
+            # a loop that iterates over each card in hand
+            for card in player_hand:
+                # uses a f-string to show value of card
+                print(f"{card['value']} of {card['suit']}")
+                # this calculates if players hand is greater than 21 = bust
+            if calculate_hand(player_hand) > 21:
+                print("You busted! Dealer wins.")
+                return player_hand
+                # if player selects "s" then current hand is held
+        elif action == 's':
+            return player_hand
+            # this ensures a valid input is entered
+        else:
+            print("Invalid input. Please enter 'h' to hit or 's' to stand.")
 
 # def calculate_winner():
 
@@ -127,9 +157,11 @@ def dealers_turn(deck, dealer_hand):
 # main function to start game
 def main():
     display_rules()
-#     create_deck()
-#     calculate_hand()
-#     deal_cards()
+    # create the deck
+    # create_deck()
+    # calculate_hand()
+    # deal_cards()
+    # players_turn()
 
 
 main()
